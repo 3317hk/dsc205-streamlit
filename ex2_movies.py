54cc695c746dab2c61c35a997c074b875e6ef07b
3317hk <3317hk@gmail.com>
2026-08-05 18:52:32 -0400

    Update ex2_movies.py

--- ex2_movies.py
+++ ex2_movies.py
@@ -3,26 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/movies.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/CT-towns-income-census-2020.csv')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)
 
 st.markdown('---')
-st.subheader('Movies by genre of origin')
+st.subheader('Income difference')
 
-origin = st.radio('Select Genre of origin', ('Adventure', 'Children', 'Animation','Drama','Comedy','Fantasy','Crime','Action','Thriller','Horror','Romance'))
+origin = st.radio('Select Income type', ('Population', 'Number of household'))
 
-if origin == 'US':
-    df = df.loc[df['origin']=='usa']
-elif origin == 'Europe':
-    df = df.loc[df['origin']=='europe']
+if origin == 'Population':
+    df = df.loc[df['origin']=='Per Capita Income']
+elif origin == 'Median Household Income':
+    df = df.loc[df['origin']=='Median Household Income']
 else:
-    df = df.loc[df['origin']=='japan']
+    df = df.loc[df['origin']=='Median Family Income']
 
 fig = plt.figure()
 ax = fig.add_subplot()
-ax.set_xlabel('mpg')
-ax.hist(df['mpg'])
+ax.set_xlabel('Median household Income')
+ax.hist(df['Per Capita Income'])
 st.pyplot(fig)
