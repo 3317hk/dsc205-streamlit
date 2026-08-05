34488e62428bc861cf209848bd7d5f61a0152c36
3317hk <3317hk@gmail.com>
2026-08-04 23:43:03 -0400

    Update ex2_movies.py

--- ex2_movies.py
+++ ex2_movies.py
@@ -3,3 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/movies.csv')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(df)
+
+st.markdown('---')
+st.subheader('Movies by genre of origin')
+
+origin = st.radio('Select Genre of origin', ('Adventure', 'Children', 'Animation','Drama','Comedy','Fantasy','Crime','Action','Thriller','Horror','Romance'))
+
+if origin == 'US':
+    df = df.loc[df['origin']=='usa']
+elif origin == 'Europe':
+    df = df.loc[df['origin']=='europe']
+else:
+    df = df.loc[df['origin']=='japan']
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_xlabel('mpg')
+ax.hist(df['mpg'])
+st.pyplot(fig)
