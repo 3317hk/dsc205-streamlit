61deaaec55096815aa1605e82e074814ed68d858
3317hk <3317hk@gmail.com>
2026-08-05 21:14:45 -0400

    Update ex2_movies.py

--- ex2_movies.py
+++ ex2_movies.py
@@ -1,28 +1,27 @@
-import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
 
-st.title('Income by type of place')
-df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/CT-towns-income-census-2020.csv')
+st.title("Income by type of place")
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/CT-towns-income-census-2020.csv"
+)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(df)
+if st.checkbox("Show raw data"):
+  st.subheader("Raw data")
+  st.write(df)
 
-st.markdown('---')
-st.subheader('Income by the type of place')
+st.markdown("---")
+st.subheader("Income by the type of place")
 
-origin = st.radio('Select type of place', ('Town', 'City', 'Borough'))
+# Match the exact casing in your CSV ('town', 'City', 'Borough')
+origin = st.radio("Select type of place", ("town", "City", "Borough"))
 
-if origin == 'Town':
-    df = df.loc[df['Type']=='town']
-elif origin == 'City':
-    df = df.loc[df['Type']=='City']
-else:
-    df = df.loc[df['Type']=='Borough']
+# Filter using the matching case
+df_filtered = df.loc[df["Type"] == origin]
 
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.set_xlabel('type')
-ax.hist(df['type'])
+fig, ax = plt.subplots()
+ax.set_xlabel("Type")
+# Use a numeric or categorical column for the histogram, or bar chart if counting
+ax.hist(df_filtered["Per Capita Income"])  # Assuming you want to plot income
 st.pyplot(fig)
